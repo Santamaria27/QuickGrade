6c18fa81554459f1ba92c7457d59aeae5833f69a
Aishwarya TS <aishwaryats098@gmail.com>
2024-04-22 10:16:39 +0530

    Login authentication done

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -3,9 +3,36 @@ import React from 'react';
 import './Login.css'; // Import Login styles if needed
 import email_icon from '../../assets/email.png'
 import password_icon from '../../assets/password.png'
+import { useState } from "react";
+//firebase imports
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebaseConfig1"
+import { Navigate } from "react-router-dom";
+
+
 
 
 const Login = () => {
+  const [error, setError]=useState(false);
+  const [email, setEmail]=useState("");
+  const [password, setPassword]=useState("");
+
+  const handleLogin=(e)=>{
+    e.preventDefault();
+
+    signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    console.log(user);
+    <Navigate to="/" replace />
+    
+  })
+  .catch((error) => {
+    setError(true);
+  });
+  }
+
   return (
     <div className='container'>
       <div className='header'>
@@ -16,17 +43,18 @@ const Login = () => {
       <div className="inputs">
         <div className='input'>
         <img src={email_icon} alt="" />
-          <input type="email" placeholder='Email Id' />
+          <input type="email" placeholder='Email Id' onChange={e=>setEmail(e.target.value)}/>
         </div>
 
         <div className='input'>
         <img src={password_icon} alt="" />
-          <input type="password" placeholder='Password' />
+          <input type="password" placeholder='Password' onChange={e=>setPassword(e.target.value)}/>
         </div>
       </div>
 
       <div className='submit-container'>
-        <div className='submit'>Login</div>
+        <button type='submit' className='submit'onClick={handleLogin} >Login</button>
+        {error && <span>Wrong email or password</span>}
       </div>
     </div>
   );
