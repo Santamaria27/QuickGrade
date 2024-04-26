95e56d9f925d5e5657d41d9f7f9d79ccac2f4d50
J ATULYA <109218299+jatulya@users.noreply.github.com>
2024-04-26 15:47:07 +0530

    Updated

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -4,15 +4,7 @@ import {getFirestore} from '@firebase/firestore'
 import { getStorage } from 'firebase/storage'
 
 // add firebaseConfig here.
-const firebaseConfig = {
-    apiKey: "AIzaSyBQBDMos_pWfkAPf2hZBUbk77hqRQi1kaU",
-    authDomain: "gradeease-57107.firebaseapp.com",
-    projectId: "gradeease-57107",
-    storageBucket: "gradeease-57107.appspot.com",
-    messagingSenderId: "778160352595",
-    appId: "1:778160352595:web:94a9ee93164131a242c5a6",
-    measurementId: "G-F70FM48GS2"
-  };
+
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app)
